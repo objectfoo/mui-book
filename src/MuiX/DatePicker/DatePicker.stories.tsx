cca9d9acdc64addebe2cf325b5c29da50bee6200
objectfoo <objectfoo@users.noreply.github.com>
2025-08-08 12:02:58 -0700

    update sidebar structure, add date picker sample

--- src/MuiX/DatePicker/DatePicker.stories.tsx
+++ src/MuiX/DatePicker/DatePicker.stories.tsx
@@ -1,6 +1,12 @@
-import type { Meta, StoryObj } from "@storybook/react-vite";
 import { DatePicker, type DatePickerProps } from "./DatePicker";
+import { MobileDatePicker } from "./MobileDatePicker";
+import { StaticDatePicker } from "./StaticDatePicker";
 import { WithDatePickerLocalizationProvider } from "./WithLocalizationProvider";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import Typography from "@mui/material/Typography";
+
 
 const meta = {
 	title: "mui-x/Date Pickers/DatePicker",
@@ -9,10 +15,13 @@ const meta = {
 	parameters: {},
 	argTypes: {
 		label: { type: "string" },
+		disabled: { control: { type: "radio" }, options: [true, false] },
+		readOnly: { control: { type: "radio" }, options: [true, false] },
 	},
 	decorators: [
 		WithDatePickerLocalizationProvider,
 	],
+	subcomponents: { MobileDatePicker, StaticDatePicker }
 } satisfies Meta<DatePickerProps>;
 
 
@@ -22,6 +31,28 @@ export type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
 	args: {
-		label: "label",
+		label: "Date picker",
+		disabled: false,
+		readOnly: false,
 	},
+	render: (args) => {
+		return (
+			<Stack direction="column" spacing={5} alignItems="flex-start">
+				<Stack direction="column" spacing={1.5}>
+					<Typography variant="h5" component="h2">Default</Typography>
+					<DatePicker {...args} />
+				</Stack>
+				<Stack direction="column" spacing={1.5}>
+					<Typography variant="h5" component="h2">Mobile</Typography>
+					<MobileDatePicker {...args} />
+				</Stack>
+				<Stack direction="column" spacing={1.5}>
+					<Typography variant="h5" component="h2">Static</Typography>
+					<Paper variant="outlined">
+						<StaticDatePicker {...args} />
+					</Paper>
+				</Stack>
+			</Stack>
+		);
+	}
 };
--- src/MuiX/DatePicker/DatePicker.tsx
+++ src/MuiX/DatePicker/DatePicker.tsx
@@ -1,13 +1,13 @@
 import { DatePicker as MuiDatePicker, type DatePickerProps as MuiDatePickerProps } from "@mui/x-date-pickers";
+import { DateTime } from "luxon";
 import { type ReactNode } from "react";
 
-
-type DatePickerBaseProps = Pick<MuiDatePickerProps, "desktopModeMediaQuery" | "label">;
+type DatePickerBaseProps = Pick<MuiDatePickerProps, "label" | "disabled" | "readOnly">;
 
 export interface DatePickerProps extends DatePickerBaseProps {
 	label: string;
 }
 
 export function DatePicker({ ...rest }: DatePickerProps): ReactNode {
-	return <MuiDatePicker {...rest} />;
+	return <MuiDatePicker {...rest} defaultValue={DateTime.local()} />;
 }
